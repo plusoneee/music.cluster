6e55f002c581208d18ca6a0e4380776561e6fd89
plusoneee <y02121148s@gmail.com>
2019-04-11 15:29:41 +0800

    edit

--- main.py
+++ main.py
@@ -8,21 +8,23 @@ class Main():
         self.sp = spotify_auth()
         self.playlists = self.sp.user_playlists('plusoneee')
         self.m_extract = Extracter(self.sp)
-        self.m_extract.write_csv_header()
+        
 
     def run(self):
         self.extrack_each_song_by_extracter()
 
     def extrack_each_song_by_extracter(self):
         for item in self.playlists['items']:
-            # if item['name'] == 'forClustering':
+            file_name = item['name']
+            file_path = './data/'+ file_name + '.csv'
+            self.m_extract.write_csv_header(file_path)
             results = self.sp.user_playlist('plusoneee', item['id'], fields="tracks, next")
             tracks = results['tracks']
-            self.m_extract.show_tracks(tracks)
+            self.m_extract.show_tracks(tracks, file_name)
             while tracks['next']:
                 tracks = self.sp.next(tracks)
-                self.m_extract.show_tracks(tracks)
-
+                self.m_extract.show_tracks(tracks, file_name)
+            
 if __name__ == '__main__':
     obj = Main()
     obj.run()
